93a0a854c4cb3213fe023ab492d08560982e79bf
ssyyzc_001 <ssyyzc_001@163.com>
2025-11-04 11:23:55 +0800

    1

---
+++ assets/core/utils/TweenUtil.ts
@@ -0,0 +1,26 @@
+import { Node } from "cc";
+import { ITweenOption } from "cc";
+import { __private } from "cc";
+import { tween } from "cc";
+import { Vec3 } from "cc";
+
+/** 动画工具 */
+export class TweenUtil {
+    /**
+     * 间隔天数
+     * @param time1 开始时间
+     * @param time2 结束时间
+     * @returns 
+     */
+
+    static to(node: Node, duration: number, props: __private._cocos_tween_tween__ConstructorType<Node>, opts?: ITweenOption<Node> | undefined){
+        return new Promise((resolve, reject) => {
+            tween(node)
+            .to(duration, props, opts)
+            .call(() => {
+                resolve(null)
+            })
+            .start()
+        })
+    }
+}

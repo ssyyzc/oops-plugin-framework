9ae7626dd973bb9f04807395df5f79bf29814b1f
ssyyzc_001 <ssyyzc_001@163.com>
2025-12-17 10:48:16 +0800

    1

--- assets/libs/collection/SimpleQueue.ts
+++ assets/libs/collection/SimpleQueue.ts
@@ -6,11 +6,15 @@ export class SimpleQueue {
         this.wait_count = wait_count || 0
     }
 
-    async push(fun : Function) {
-        this.ps.push(new Promise(async (resolve, reject) => {
-            await fun()
-            resolve()
-        }))
+    async push(fun : Function | Promise<void>) {
+        if(fun instanceof Promise){
+            this.ps.push(fun)
+        }else{
+            this.ps.push(new Promise(async (resolve, reject) => {
+                await fun()
+                resolve()
+            }))
+        }
 
         //到达检测数量时，自动等待所有任务完成
         if(this.wait_count > 0 && this.ps.length >= this.wait_count){
